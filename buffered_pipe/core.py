62d911124917dd39be9b23c333ac60157cfe719c
klae01 <tspt2479@gmail.com>
2022-03-22 13:37:05 +0900

    python POC

--- buffered_pipe/core.py
+++ buffered_pipe/core.py
@@ -1,18 +1,12 @@
-import os
 import pickle
 import threading
+import multiprocessing
+from multiprocessing.shared_memory import SharedMemory
+from multiprocessing import resource_tracker
 import copy
-import random
+import collections
 from typing import Tuple, Any
 
-from .module import init_sema, create_shm, delete_shm, create_pipe, send_bytes, recv_bytes
-
-def random_string(length = 5, use_upper = False, use_lower = True, use_number = True):
-    set = []
-    if use_upper: set += list(range(ord('A'),ord('Z') + 1))
-    if use_lower: set += list(range(ord('a'),ord('z') + 1))
-    if use_number: set += list(range(ord('0'),ord('9') + 1))
-    return ''.join(map(chr, random.choices(set, k = length)))
 class connection:
     def recv_bytes(self) -> bytes: ...
     def send_bytes(self, data: bytes) -> None: ...
@@ -21,52 +15,132 @@ class connection:
 
 class SPipe(connection):
     lock = threading.RLock()
-    @staticmethod
-    def __random_sema_create(format):
-        while True:
-            s = random_string(5, use_upper = False, use_lower = True, use_number = True)
-            X = format.format(s)
-            if not init_sema((X, )):
-                break
-        return X
     def __init__(self, minimum_write = 64, size = 2 ** 25):
         with SPipe.lock:
-            self.shm_id = create_shm((size, ))
-            print("shm_id", self.shm_id)
-            self.size = size
-
             self.minimum_write = minimum_write
-            sema_fn_base = f"BP_{os.getpid()}_{id(self)}"
-            self.sema1_fn = SPipe.__random_sema_create(sema_fn_base+"_1_{}.sema")
-            self.sema2_fn = SPipe.__random_sema_create(sema_fn_base+"_2_{}.sema")
-            print(self.sema1_fn)
-            print(self.sema2_fn)
-            self.pipe_id = None
+            self.size = size
+            self.shm = SharedMemory(create=True, size = size)
+            resource_tracker.unregister( self.shm._name, 'shared_memory')
+            self.resource_owner = None
+            self.Plock = multiprocessing.Lock()
+            self.sem_a = multiprocessing.Semaphore(0)
+            self.sem_f = multiprocessing.Semaphore(0)
             self.mode = None
+            self.is_init = False
+    def __read_request_bytes(self, length):
+        result = b""
+        if self.size < self.__pointer + length:
+            result += self.shm.buf[self.__pointer:]
+            length -= self.size - self.__pointer
+            self.__pointer = 0
+        result += self.shm.buf[self.__pointer:self.__pointer + length]
+        self.__pointer += length
+        return result
+    def __read_request(self, length, result):
+        # shared memory 
+        if self.size < self.__pointer + length:
+            result.append(bytes(self.shm.buf[self.__pointer:]))
+            length -= self.size - self.__pointer
+            self.__pointer = 0
+        result.append(bytes(self.shm.buf[self.__pointer:self.__pointer + length]))
+        self.__pointer += length
+    def __write_request(self, data):
+        if self.size <= self.__pointer + len(data):
+            split = self.size - self.__pointer
+            self.shm.buf[self.__pointer:] = data[:split]
+            self.__pointer = len(data) - (self.size - self.__pointer)
+            if self.__pointer:
+                self.shm.buf[:self.__pointer] = data[split:]
+        else:
+            self.shm.buf[self.__pointer:self.__pointer + len(data)] = data
+            self.__pointer += len(data)
+    # def __write_request(self, data):
+    #     if self.size < self.__pointer + len(data):
+    #         split = self.size - self.__pointer
+    #         self.shm.buf[self.__pointer:] = data[:split]
+    #         data = data[split:]
+    #         self.__pointer = 0
+    #     self.shm.buf[self.__pointer:self.__pointer + len(data)] = data
+    #     self.__pointer += len(data)
+    @staticmethod
+    def get_id():
+        # return multiprocessing.current_process().ident
+        return (multiprocessing.current_process().ident, threading.current_thread().ident)
+    def init(self):
+        if self.resource_owner == None:
+            self.resource_owner = SPipe.get_id()
+        assert self.resource_owner == SPipe.get_id()
+        if not self.is_init:
+            self.__pointer = 0
+            self.lock = threading.RLock()
+            if self.mode == 'W':
+                self.deque = collections.deque()
+                self.margin = self.size
+            self.is_init = True
+
     def set_mode(self, mode):
         assert self.mode is None
         assert mode in ['R', 'W']
         self.mode = mode
-    def init(self):
-        if self.pipe_id is None:
-            assert self.mode is not None
-            self.lock = threading.RLock()
-            self.pipe_id = create_pipe((self.sema1_fn, self.sema2_fn, self.shm_id, self.size, self.minimum_write))
-            print("get pipe id", self.pipe_id)
+    
     def recv_bytes(self):
         assert 'R' == self.mode
         self.init()
+        FLAG = True
+        result = []
         with self.lock:
-            return recv_bytes((self.pipe_id, ))
+            while FLAG:
+                self.sem_a.acquire()
+                length = self.__read_request_bytes(4)
+                # print(f"recv length info = ", length)
+                length = int.from_bytes(length, 'big', signed = True)
+                # print(f"recv length = {length}, sem_a = {self.sem_a.get_value()}, sem_f = {self.sem_f.get_value()} ")
+                self.__read_request(abs(length), result)
+                FLAG = length < 0
+                self.sem_f.release()
+        # print(f"recv pointer = {self.__pointer}, data_len = {sum(map(len, result))}, data_spans = {len(result)}")
+        # print(f"recv data = ", b''.join(result))
+        return b''.join(result)
+        
     def send_bytes(self, data):
         assert 'W' == self.mode
         self.init()
         with self.lock:
-            send_bytes((self.pipe_id, data))
+            data_remain = len(data)
+            data_pointer = 0
+            while(data_remain):
+                critical_length = data_remain + 4
+                while(self.margin < critical_length and self.sem_f.acquire(block=False)):
+                    self.margin += self.deque.popleft()
+                loop_end_condition = min(self.minimum_write, critical_length)
+                while(self.margin < loop_end_condition):
+                    self.sem_f.acquire()
+                    self.margin += self.deque.popleft()
+                write_length = min(self.margin, critical_length)
+                self.deque.append(write_length)
+                self.margin -= write_length
+
+                write_length -= 4
+                data_info = write_length if data_remain == write_length else -write_length
+                self.__write_request(data_info.to_bytes(4, 'big', signed=True))
+                self.__write_request(data[data_pointer:data_pointer+write_length])
+                data_remain -= write_length
+                data_pointer += write_length
+
+                self.sem_a.release()
+        # print(f"send margin = {self.margin}, pointer = {self.__pointer}, data_len = {len(data)}")
+        # mem_state = ' '.join(f'{n:02x}' for n in self.shm.buf)
+        # print(f"send memory change: {mem_state}")
     def recv(self) -> Any:
         return pickle.loads(self.recv_bytes())
     def send(self, item):
         self.send_bytes(pickle.dumps(item))
+
+    def __del__(self):
+        with self.Plock:
+            if self.resource_owner == self.get_id() and self.mode == 'R':
+                print("DELETE")
+                self.shm.close()
 
 class BPipe(connection):
     def __init__(self, minimum_write=64, size=2 ** 25):
--- setup.py
+++ setup.py
@@ -3,10 +3,7 @@ from distutils.core import setup, Extension, DEBUG
  
 # from setuptools import find_packages, setup, Command
 
-sfc_module = Extension('buffered_pipe.module', sources = ['cpp/module.cpp'])
-
 setup(name = 'buffered_pipe', version = '0.0.0',
     description = 'Buffered pipe through shared memory.',
-    packages=['buffered_pipe'],
-    ext_modules = [sfc_module]
+    packages=['buffered_pipe']
 )
