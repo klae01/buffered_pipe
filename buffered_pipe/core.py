af857bceee8ce9f5328ea61974dcdfcdc3304640
klae01 <tspt2479@gmail.com>
2022-03-27 19:33:45 +0900

    init

--- buffered_pipe/core.py
+++ buffered_pipe/core.py
@@ -5,30 +5,7 @@ import random
 import threading
 from typing import Any, Tuple
 
-from .module import create_sem, delete_sem, attach_sem, detach_sem
-from .module import create_shm, delete_shm, attach_shm, detach_shm
-from .module import recv_bytes, send_bytes
-from .module import getpid
-
-
-def _random_string(length=5, use_upper=False, use_lower=True, use_number=True):
-    set = []
-    if use_upper:
-        set += list(range(ord("A"), ord("Z") + 1))
-    if use_lower:
-        set += list(range(ord("a"), ord("z") + 1))
-    if use_number:
-        set += list(range(ord("0"), ord("9") + 1))
-    return "".join(map(chr, random.choices(set, k=length)))
-
-
-def _random_sema_create(format):
-    while True:
-        s = _random_string(5, use_upper=False, use_lower=True, use_number=True)
-        X = format.format(s)
-        if not create_sem((X,)):
-            break
-    return X
+from .module import init, free, recv_bytes, send_bytes
 
 
 class connection:
@@ -46,94 +23,15 @@ class connection:
 
 
 class SPipe(connection):
-    lock = threading.Lock()
-
     def __init__(self, minimum_write, size):
-        with SPipe.lock:
-            self.minimum_write = minimum_write
-            self.shm_size = size
-
-            self.shm_id = create_shm((self.shm_size,))
-            # print("shm_id", self.shm_id)
-
-            sema_fn_base = f"BP_{getpid()}_{id(self)}"
-            self.sema1_fn = _random_sema_create(sema_fn_base + "_1_{}.sema")
-            self.sema2_fn = _random_sema_create(sema_fn_base + "_2_{}.sema")
-            # print(self.sema1_fn)
-            # print(self.sema2_fn)
-
-            self.shm_ptr = None
-            self.sema1_ptr = None
-            self.sema2_ptr = None
-            self.resource_owner = None
-
-            self.mode = None
-
-    def set_mode(self, mode):
-        assert self.mode is None
-        assert mode in ["R", "W"]
-
-        self.mode = mode
-        if self.mode == "R":
-            self.pointer = 0
-        elif self.mode == "W":
-            self.pointer_a = 0
-            self.pointer_f = 0
-            self.margin = self.shm_size
-
-        self.lock = multiprocessing.Lock()
-
-    def init(self):
-        current_id = getpid()
-        if self.resource_owner != current_id:
-            # print("SPIPE ID", current_id)
-            self.resource_owner = current_id
-            assert self.mode is not None
-
-            self.shm_ptr = attach_shm((self.shm_id,))
-            self.sema1_ptr = attach_sem((self.sema1_fn,))
-            self.sema2_ptr = attach_sem((self.sema2_fn,))
-
-            # print("shm_ptr:", self.shm_ptr)
-            # print("sem1_ptr:", self.sema1_ptr)
-            # print("sem2_ptr:", self.sema2_ptr)
+        self.fd_pipe = init((minimum_write, size))
 
     def recv_bytes(self):
-        # shm_buf, shm_size, sem_a, sem_f, pointer
-        # return pointer, data
-        assert "R" == self.mode
-        with self.lock:
-            self.init()
-            self.pointer, data = recv_bytes(
-                (
-                    self.shm_ptr,
-                    self.shm_size,
-                    self.sema1_ptr,
-                    self.sema2_ptr,
-                    self.pointer,
-                )
-            )
-        return b"".join(data)
+        self.fd_pipe, result = recv_bytes(self.fd_pipe)
+        return result
 
     def send_bytes(self, data):
-        # shm_buf, shm_size, sem_a, sem_f, pointer_a, pointer_f, margin, data
-        # return pointer_a, pointer_f, margin
-        assert "W" == self.mode
-        with self.lock:
-            self.init()
-            self.pointer_a, self.pointer_f, self.margin = send_bytes(
-                (
-                    self.shm_ptr,
-                    self.shm_size,
-                    self.sema1_ptr,
-                    self.sema2_ptr,
-                    self.minimum_write,
-                    self.pointer_a,
-                    self.pointer_f,
-                    self.margin,
-                    data,
-                )
-            )
+        self.fd_pipe = send_bytes((self.fd_pipe, data))
 
     def recv(self):
         return pickle.loads(self.recv_bytes())
@@ -142,25 +40,8 @@ class SPipe(connection):
         self.send_bytes(pickle.dumps(item))
 
     def __del__(self):
-        if self.resource_owner == getpid():
-            if self.shm_ptr:
-                detach_shm((self.shm_ptr,))
-                self.shm_ptr = None
-            if self.shm_id:
-                delete_shm((self.shm_id,))
-                self.shm_id = None
-            if self.sema1_ptr:
-                detach_sem((self.sema1_ptr,))
-                self.sema1_ptr = None
-            if self.sema1_fn:
-                delete_sem((self.sema1_fn,))
-                self.sema1_fn = None
-            if self.sema2_ptr:
-                detach_sem((self.sema2_ptr,))
-                self.sema2_ptr = None
-            if self.sema2_fn:
-                delete_sem((self.sema2_fn,))
-                self.sema2_fn = None
+        if self.fd_pipe:
+            self.fd_pipe = free(self.fd_pipe)
 
 
 class BPipe(connection):
@@ -173,8 +54,6 @@ class BPipe(connection):
         assert self.mode is None
         assert mode in ["RW", "WR"]
         self.mode = mode
-        self.pipe_1.set_mode(self.mode[0])
-        self.pipe_2.set_mode(self.mode[1])
 
     def recv_bytes(self):
         pipe = [self.pipe_1, self.pipe_2][self.mode[0] == "W"]
@@ -202,8 +81,5 @@ def Pipe(
         pipe_2.set_mode("WR")
         return pipe_1, pipe_2
     else:
-        pipe_r = SPipe(minimum_write, size)
-        pipe_w = copy.copy(pipe_r)
-        pipe_r.set_mode("R")
-        pipe_w.set_mode("W")
-        return pipe_r, pipe_w
+        pipe = SPipe(minimum_write, size)
+        return pipe, pipe
--- cpp/module.cpp
+++ cpp/module.cpp
@@ -9,124 +9,131 @@
 #include <semaphore.h>
 #include <string.h>
 
-const int LENGTH_INFO = 4;
-typedef long long OP_TYPE;
-typedef long long BASE_TYPE;
-typedef long long PTR_LOAD_TYPE;
 typedef uintptr_t PTR_TYPE;
 
 #define PTR2LL(x) (long long)(uintptr_t)(x)
 #define LL2PTR(x) (void *)(uintptr_t)(x)
 
-// create_sem, delete_sem, attach_sem, detach_sem
-// create_shm, delete_shm, attach_shm, detach_shm
+// init, free
 // recv_bytes, send_bytes
-// getpid
 
-PyObject* _getpid(PyObject *) {
-    return PyLong_FromUnsignedLongLong(getpid());
+struct Pipe_info {
+    // placed on shared memory
+    unsigned int minimum_write;
+    unsigned int shm_size;
+    unsigned int LENGTH_INFO;
+    unsigned int buf_id;
+    unsigned int pointer;
+    unsigned int pointer_a;
+    unsigned int pointer_f;
+    unsigned int margin;
+    sem_t sem_a, sem_f;
+    pthread_mutex_t mutex_w, mutex_r;
+};
+
+struct Pipe {
+    // placed on private memory, save on python
+    Pipe_info *info;
+    char *shm_buf;
+    
+    unsigned int info_id;
+    pid_t pid;
+};
+
+void mp_reinit(Pipe &pipe) {
+    pipe.info = (Pipe_info*) shmat(pipe.info_id, NULL, 0);
+    pipe.shm_buf = (char*) shmat(pipe.info->buf_id, NULL, 0);
+    pipe.pid = getpid();
 }
 
-PyObject* create_sem(PyObject *, PyObject* args) {
-    char *sem_name;
-    PyArg_ParseTuple(args, "s", &sem_name);
-    sem_t *sem = sem_open(sem_name, O_CREAT | O_EXCL, 0644, 0);
-    if(!sem) sem_close(sem);
-    return PyLong_FromLongLong(sem == SEM_FAILED);
+void mp_request_init(Pipe &pipe) {
+    if(pipe.pid != getpid())
+        mp_reinit(pipe);
 }
 
-PyObject* delete_sem(PyObject *, PyObject* args) {
-    char *sem_name;
-    BASE_TYPE result;
-    PyArg_ParseTuple(args, "s", &sem_name);
-    result = sem_unlink(sem_name);
-    return PyLong_FromLongLong(result);
+PyObject* init(PyObject *, PyObject* args) {
+    // 0: shm size-> shared memory ids
+    // 1: pipe id -> info ptr, buf ptr
+    Pipe pipe;
+    unsigned int minimum_write, shm_size;
+    PyArg_ParseTuple(args, "II", &minimum_write, &shm_size);
+    pipe.info_id = shmget(IPC_PRIVATE, sizeof(Pipe_info), IPC_CREAT | 0644);
+    pipe.info = (Pipe_info*) shmat(pipe.info_id, NULL, 0);
+
+    pipe.info->buf_id = shmget(IPC_PRIVATE, shm_size, IPC_CREAT | 0644);
+    memset(pipe.info, 0, sizeof(Pipe_info));
+    pipe.info->minimum_write = minimum_write;
+    pipe.info->shm_size = shm_size;
+    for(unsigned int t = shm_size >> 7; t; t >>= 8)
+        pipe.info->LENGTH_INFO++;
+    pipe.info->LENGTH_INFO++;
+    pipe.info->margin = shm_size;
+
+    sem_init(&pipe.info->sem_a, 1, 0);
+    sem_init(&pipe.info->sem_f, 1, 0);
+
+    pthread_mutexattr_t psharedm;
+    pthread_mutexattr_init(&psharedm);
+    pthread_mutexattr_setpshared(&psharedm, PTHREAD_PROCESS_SHARED);
+    pthread_mutex_init(&pipe.info->mutex_w, &psharedm);
+    pthread_mutex_init(&pipe.info->mutex_r, &psharedm);
+    
+    mp_reinit(pipe);
+    return PyByteArray_FromStringAndSize((char*) &pipe, sizeof(Pipe));
 }
 
-PyObject* attach_sem(PyObject *, PyObject* args) {
-    char *sem_name;
-    PyArg_ParseTuple(args, "s", &sem_name);
-    sem_t *sem = sem_open(sem_name, O_CREAT, 0644, 0);
-    return PyLong_FromLongLong(PTR2LL(sem));
+PyObject* free(PyObject *, PyObject* args) {
+    // 0: shm size-> shared memory ids
+    // 1: pipe id -> info ptr, buf ptr
+    Pipe pipe;
+    memcpy(&pipe, PyByteArray_AsString(args), sizeof(Pipe));
+    // PyArg_ParseTuple(args, "y", (char*)&pipe);
+    shmdt(pipe.shm_buf);
+    shmctl(pipe.info->buf_id , IPC_RMID , NULL);
+    shmdt(pipe.info);
+    shmctl(pipe.info_id , IPC_RMID , NULL);
+    Py_RETURN_NONE;
 }
 
-PyObject* detach_sem(PyObject *, PyObject* args) {
-    PTR_LOAD_TYPE sem_ptr;
-    BASE_TYPE result;
-    PyArg_ParseTuple(args, "L", &sem_ptr);
-    result = sem_close((sem_t*)LL2PTR(sem_ptr));
-    return PyLong_FromLongLong(result);
-}
-
-PyObject* create_shm(PyObject *, PyObject* args) {
-    BASE_TYPE shm_size, shm_id;
-    PyArg_ParseTuple(args, "L", &shm_size);
-	shm_id = shmget(IPC_PRIVATE, shm_size, IPC_CREAT | 0644);
-    return PyLong_FromLongLong(shm_id);
-}
-
-PyObject* delete_shm(PyObject *, PyObject* args) {
-    BASE_TYPE shm_id, result;
-    PyArg_ParseTuple(args, "L", &shm_id);
-    result = shmctl(shm_id , IPC_RMID , NULL);
-    return PyLong_FromLongLong(result);
-}
-
-PyObject* attach_shm(PyObject *, PyObject* args) {
-    PTR_LOAD_TYPE shmid;
-    BASE_TYPE shmaddr;
-    PyArg_ParseTuple(args, "L", &shmid);
-    shmaddr = PTR2LL(shmat(shmid, NULL, 0));
-    return PyLong_FromLongLong(shmaddr);
-}
-
-PyObject* detach_shm(PyObject *, PyObject* args) {
-    PTR_LOAD_TYPE shmaddr;
-    BASE_TYPE result;
-    PyArg_ParseTuple(args, "L", &shmaddr);
-    result = shmdt(LL2PTR(shmaddr));
-    return PyLong_FromLongLong(result);
-}
-
-BASE_TYPE read_request(char* shm_buf, OP_TYPE shm_size, OP_TYPE pointer, OP_TYPE length, char*result) {
-    OP_TYPE lookup = 0;
-    if(shm_size < pointer + length) {
-        lookup = shm_size - pointer;
-        memmove(result, shm_buf + pointer, lookup);
+void read_request(Pipe &pipe, Pipe_info &info, unsigned int length, char*result) {
+    unsigned int lookup = 0;
+    if(info.shm_size < info.pointer + length) {
+        lookup = info.shm_size - info.pointer;
+        memmove(result, pipe.shm_buf + info.pointer, lookup);
         length -= lookup;
-        pointer = 0;
+        info.pointer = 0;
     }
-    memmove(result + lookup, shm_buf + pointer, length);
-    return pointer + length;
+    memmove(result + lookup, pipe.shm_buf + info.pointer, length);
+    info.pointer += length;
 }
 
 inline void PyList_Append_(PyObject* list, PyObject* item) {
     PyList_Append(list, item);
     Py_DECREF(item);
 }
-BASE_TYPE read_request(char* shm_buf, OP_TYPE shm_size, OP_TYPE pointer, OP_TYPE length, PyObject* result) {
-    OP_TYPE lookup = 0;
-    if(shm_size < pointer + length) {
-        lookup = shm_size - pointer;
-        if(lookup) PyList_Append_(result, PyBytes_FromStringAndSize(shm_buf + pointer, lookup));
+void read_request(Pipe &pipe, Pipe_info &info, unsigned int length, PyObject* result) {
+    unsigned int lookup = 0;
+    if(info.shm_size < info.pointer + length) {
+        lookup = info.shm_size - info.pointer;
+        if(lookup) PyList_Append_(result, PyBytes_FromStringAndSize(pipe.shm_buf + info.pointer, lookup));
         length -= lookup;
-        pointer = 0;
+        info.pointer = 0;
     }
-    if(length) PyList_Append_(result, PyBytes_FromStringAndSize(shm_buf + pointer, length));
-    return pointer + length;
+    if(length) PyList_Append_(result, PyBytes_FromStringAndSize(pipe.shm_buf + info.pointer, length));
+    info.pointer += length;
 }
 
-BASE_TYPE write_request(char* shm_buf, OP_TYPE shm_size, OP_TYPE pointer, char* data, OP_TYPE length) {
-    OP_TYPE tmp;
-    if(shm_size < pointer + length) {
-        tmp = shm_size - pointer;
-        memmove(shm_buf+pointer, data, tmp);
+void write_request(Pipe &pipe, Pipe_info &info, char* data, unsigned int length) {
+    unsigned int tmp;
+    if(info.shm_size < info.pointer + length) {
+        memmove(pipe.shm_buf+info.pointer, data, tmp);
+        tmp = info.shm_size - info.pointer;
         data += tmp;
         length -= tmp;
-        pointer = 0;
+        info.pointer = 0;
     }
-    memmove(shm_buf+pointer, data, length);
-    return pointer + length;
+    memmove(pipe.shm_buf+info.pointer, data, length);
+    info.pointer += length;
 }
 
 
@@ -145,23 +152,23 @@ union int2char {
 //     return i2c_value.c;
 // }
 
-BASE_TYPE chunk_size(char *shm_buf, OP_TYPE shm_size, OP_TYPE pointer) {
+int chunk_size(Pipe &pipe, Pipe_info &info) {
     int2char i2c_value, i2c_cast;
     i2c_value.v = 0;
-    read_request(shm_buf, shm_size, pointer, LENGTH_INFO, i2c_value.c);
+    read_request(pipe, info, info.LENGTH_INFO, i2c_value.c);
     
-    if(i2c_value.c[LENGTH_INFO - 1] < 0) {
+    if(i2c_value.c[info.LENGTH_INFO - 1] < 0) {
         i2c_cast.v = -1;
-        memmove(i2c_cast.c, i2c_value.c, LENGTH_INFO);
+        memmove(i2c_cast.c, i2c_value.c, info.LENGTH_INFO);
         return i2c_cast.v;
     }
     return i2c_value.v;
 }
 
-BASE_TYPE write_int(char *shm_buf, OP_TYPE shm_size, OP_TYPE pointer, OP_TYPE value) {
+void write_int(Pipe &pipe, Pipe_info &info, unsigned int value) {
     int2char i2c_value;
     i2c_value.v = value;
-    return write_request(shm_buf, shm_size, pointer, i2c_value.c, LENGTH_INFO);
+    write_request(pipe, info, i2c_value.c, info.LENGTH_INFO);
 }
 
 // void printf_hex(char *shm_buf, BASE_TYPE shm_size) {
@@ -171,93 +178,74 @@ BASE_TYPE write_int(char *shm_buf, OP_TYPE shm_size, OP_TYPE pointer, OP_TYPE va
 // }
 
 PyObject* recv_bytes(PyObject *, PyObject* args) {
-    // shm_buf, shm_size, sem_a, sem_f, pointer
-    // return pointer, data
-    PTR_LOAD_TYPE _shm_buf, _sem_a, _sem_f;
-    BASE_TYPE shm_size, pointer;
-    PyArg_ParseTuple(args, "LLLLL", &_shm_buf, &shm_size, &_sem_a, &_sem_f, &pointer);
-    char* shm_buf = (char*)LL2PTR(_shm_buf);
-    sem_t *sem_a = (sem_t*)LL2PTR(_sem_a), *sem_f = (sem_t*)LL2PTR(_sem_f);
-
-    int FLAG = true;
+    Pipe pipe;
+    memcpy(&pipe, PyByteArray_AsString(args), sizeof(Pipe));
+    mp_request_init(pipe);
+    Pipe_info &info = *pipe.info;
     PyObject* result = PyList_New(0);
-    BASE_TYPE length, remain;
-    while(FLAG) {
-        if(sem_trywait(sem_a)) {
+    pthread_mutex_lock(&info.mutex_r);
+    int length = -1;
+    while(length < 0) {
+        if(sem_trywait(&info.sem_a)) {
             Py_BEGIN_ALLOW_THREADS 
-            sem_wait(sem_a);
+            sem_wait(&info.sem_a);
             Py_END_ALLOW_THREADS 
         }
-
-        length = chunk_size(shm_buf, shm_size, pointer);
-        pointer = (pointer + LENGTH_INFO) % shm_size;
-        remain = std::abs(length);
-        FLAG = length < 0;
-        
-        pointer = read_request(shm_buf, shm_size, pointer, remain, result);
-
-        sem_post(sem_f);
+        length = chunk_size(pipe, info);
+        read_request(pipe, info, std::abs(length), result);
+        sem_post(&info.sem_f);
     }
-    return Py_BuildValue("LO", pointer, result);
+    pthread_mutex_unlock(&info.mutex_r);
+    return Py_BuildValue("y#O", &pipe, sizeof(Pipe), result);
 }
 
 PyObject* send_bytes(PyObject *, PyObject *args) {
-    // shm_buf, shm_size, sem_a, sem_f, minimum_write, pointer_a, pointer_f, margin, data
-    // return pointer_a, pointer_f, margin
+    Pipe pipe;
     char*      req_data;
     Py_ssize_t req_len = 0;
-    PTR_LOAD_TYPE _shm_buf, _sem_a, _sem_f;
-    BASE_TYPE shm_size, minimum_write, pointer_a, pointer_f, margin;
-    PyArg_ParseTuple(args, "LLLLLLLLy#", &_shm_buf, &shm_size, &_sem_a, &_sem_f, &minimum_write, &pointer_a, &pointer_f, &margin, &req_data, &req_len);
-    char* shm_buf = (char*)LL2PTR(_shm_buf);
-    sem_t *sem_a = (sem_t*)LL2PTR(_sem_a), *sem_f = (sem_t*)LL2PTR(_sem_f);
-    
-    OP_TYPE critical_length, write_length, free_length, minimum_length;
+    PyArg_ParseTuple(args, "yy#", (char*)&pipe, &req_data, &req_len);
+    mp_request_init(pipe);
+    Pipe_info &info = *pipe.info;
+
+    pthread_mutex_lock(&info.mutex_w);
+    unsigned int critical_length, write_length, free_length, minimum_length;
     while(req_len) {
-        critical_length = req_len + LENGTH_INFO;
-        minimum_length = std::min((OP_TYPE)minimum_write, critical_length);
-        while(margin < critical_length && !sem_trywait(sem_f)) {
-            free_length = LENGTH_INFO + std::abs(chunk_size(shm_buf, shm_size, pointer_f));
-            margin += free_length;
-            pointer_f = (pointer_f + free_length) % shm_size;
+        critical_length = req_len + info.LENGTH_INFO;
+        minimum_length = std::min(info.minimum_write, critical_length);
+        while(info.margin < critical_length && !sem_trywait(&info.sem_f)) {
+            free_length = std::abs(chunk_size(pipe, info));
+            info.margin += info.LENGTH_INFO + free_length;
+            info.pointer_f = (info.pointer_f + free_length) % info.shm_size;
         }
-        while(margin < minimum_length) {
+        while(info.margin < minimum_length) {
             Py_BEGIN_ALLOW_THREADS 
-            sem_wait(sem_f);
+            sem_wait(&info.sem_f);
             Py_END_ALLOW_THREADS 
-            free_length = LENGTH_INFO + std::abs(chunk_size(shm_buf, shm_size, pointer_f));
-            margin += free_length;
-            pointer_f = (pointer_f + free_length) % shm_size;
+            free_length = std::abs(chunk_size(pipe, info));
+            info.margin += info.LENGTH_INFO + free_length;
+            info.pointer_f = (info.pointer_f + free_length) % info.shm_size;
         }
         
-        write_length = std::min((OP_TYPE)margin, critical_length);
-        margin -= write_length;
-        write_length -= LENGTH_INFO;
-        if( write_length < req_len )
-            pointer_a = write_int(shm_buf, shm_size, pointer_a, -write_length);
-        else
-            pointer_a = write_int(shm_buf, shm_size, pointer_a, write_length);
-        pointer_a = write_request(shm_buf, shm_size, pointer_a, req_data, write_length);
+        write_length = std::min(info.margin, critical_length);
+        info.margin -= write_length;
+        write_length -= info.LENGTH_INFO;
+        if( write_length < req_len ) write_int(pipe, info, -write_length);
+        else write_int(pipe, info, write_length);
+        write_request(pipe, info, req_data, write_length);
         req_data += write_length;
         req_len -= write_length;
-        sem_post(sem_a);
+        sem_post(&info.sem_a);
     }
-    return Py_BuildValue("LLL", pointer_a, pointer_f, margin);
+    pthread_mutex_unlock(&info.mutex_w);
+    return PyByteArray_FromStringAndSize((char*) &pipe, sizeof(Pipe));
 }
 static PyMethodDef methods[] = {
     // The first property is the name exposed to Python, fast_tanh, the second is the C++
     // function name that contains the implementation.
-    { "create_sem", (PyCFunction)create_sem, METH_O, nullptr },
-    { "delete_sem", (PyCFunction)delete_sem, METH_O, nullptr },
-    { "attach_sem", (PyCFunction)attach_sem, METH_O, nullptr },
-    { "detach_sem", (PyCFunction)detach_sem, METH_O, nullptr },
-    { "create_shm", (PyCFunction)create_shm, METH_O, nullptr },
-    { "delete_shm", (PyCFunction)delete_shm, METH_O, nullptr },
-    { "attach_shm", (PyCFunction)attach_shm, METH_O, nullptr },
-    { "detach_shm", (PyCFunction)detach_shm, METH_O, nullptr },
+    { "init", (PyCFunction)init, METH_O, nullptr },
+    { "free", (PyCFunction)free, METH_O, nullptr },
     { "recv_bytes", (PyCFunction)recv_bytes, METH_O, nullptr },
     { "send_bytes", (PyCFunction)send_bytes, METH_O, nullptr },
-    { "getpid", (PyCFunction)_getpid, METH_NOARGS, nullptr },
  
     // Terminate the array with an object containing nulls.
     { nullptr, nullptr, 0, nullptr }
