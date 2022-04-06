7c3986b44c1091748d388d4a8c7211064145dc96
klae01 <tspt2479@gmail.com>
2022-04-06 18:14:25 +0900

    update format

--- buffered_pipe/generic_pipe.py
+++ buffered_pipe/generic_pipe.py
@@ -5,35 +5,21 @@ from .utils import get_duplex_Pipe
 from .generic_module import init, free, recv_bytes, send_bytes
 
 
-class connection:
-    def recv_bytes(self) -> bytes:
-        ...
-
-    def send_bytes(self, data: bytes) -> None:
-        ...
-
-    def recv(self) -> Any:
-        ...
-
-    def send(self, item: bytes) -> None:
-        ...
-
-
-class _Pipe(connection):
+class _Pipe:
     def __init__(self, minimum_write, size):
         self.fd_pipe = init((minimum_write, size))
 
-    def recv_bytes(self):
+    def recv_bytes(self) -> bytes:
         self.fd_pipe, result = recv_bytes(self.fd_pipe)
         return b''.join(result)
 
-    def send_bytes(self, data):
+    def send_bytes(self, data: bytes) -> None:
         self.fd_pipe = send_bytes((self.fd_pipe, data))
 
-    def recv(self):
+    def recv(self) -> Any:
         return pickle.loads(self.recv_bytes())
 
-    def send(self, item):
+    def send(self, item) -> None:
         self.send_bytes(pickle.dumps(item))
 
     def __del__(self):
@@ -42,7 +28,7 @@ class _Pipe(connection):
 
 def Pipe(
     minimum_write: int = 64, size: int = 2 ** 16, duplex: bool = False
-) -> Tuple[connection, connection]:
+) -> Tuple[_Pipe, _Pipe]:
     assert minimum_write <= size
     if duplex:
         get_duplex_Pipe(lambda : _Pipe(minimum_write, size))
