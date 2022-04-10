b5ec0aee2cb0d8b5fe3747e07f330457bc6efc9d
klae01 <tspt2479@gmail.com>
2022-04-10 16:52:49 +0900

    Update pipe initialization parameters

--- buffered_pipe/static_pipe.py
+++ buffered_pipe/static_pipe.py
@@ -5,14 +5,18 @@ from .static_module import init, free, recv_bytes, send_bytes
 
 
 class _Pipe:
-    def __init__(self, object_size, object_count, concurrency, polling):
+    def __init__(self, object_size, object_count, SMT_recv, SMT_send, polling):
         assert object_size > 0
         assert object_count > 0
-        assert concurrency > 0
+        assert SMT_recv == SMT_send  # version 0.1.0
+        assert SMT_recv >= 0
+        assert SMT_send >= 0
         assert polling < 1
-        if concurrency == 1:
-            concurrency = 0
-        self.fd_pipe = init((object_size, object_count, concurrency, polling))
+        SMT_recv = min(object_count, SMT_recv)
+        SMT_send = min(object_count, SMT_send)
+        SMT_recv = 0 if SMT_recv <= 1 else SMT_recv
+        SMT_send = 0 if SMT_send <= 1 else SMT_send
+        self.fd_pipe = init((object_size, object_count, SMT_recv, polling))
         self.recv = self.recv_bytes
         self.send = self.send_bytes
 
@@ -25,12 +29,19 @@ class _Pipe:
     def __del__(self):
         free(self.fd_pipe)
 
+
 def Pipe(
-    object_size: int, object_count: int, concurrency: int = 16, polling: float = 0.1, duplex = False
+    object_size: int,
+    object_count: int,
+    SMT_recv: int = 16,
+    SMT_send: int = 16,
+    polling: float = 0.1,
+    duplex: bool = False,
 ) -> Tuple[_Pipe, _Pipe]:
-    concurrency = min(object_count, concurrency)
     if duplex:
-        get_duplex_Pipe(lambda : _Pipe(object_size, object_count, concurrency, polling))
+        get_duplex_Pipe(
+            lambda: _Pipe(object_size, object_count, SMT_recv, SMT_send, polling)
+        )
     else:
-        pipe = _Pipe(object_size, object_count, concurrency, polling)
+        pipe = _Pipe(object_size, object_count, SMT_recv, SMT_send, polling)
         return pipe, pipe
